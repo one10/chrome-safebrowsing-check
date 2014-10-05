9c2fff131edd9b6f1cd9e62234214e15c4923e8f
one10 <ivan.balepin@gmail.com>
2014-10-05 00:52:50 -0700

    Update extra_checks.js

--- extra_checks.js
+++ extra_checks.js
@@ -3,3 +3,5 @@
 
 // push some extra checks to the blacklistData array here
 
+// this file is specifically for checks that don't belong in public git, so:
+// git update-index --assume-unchanged extra_checks.js
